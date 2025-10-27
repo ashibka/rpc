bf7985917416dd558b77764c556d1e515eff1d50
Ses <seryozha1209@mail.com>
2025-10-27 22:12:56 +0300

    env work norm

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,17 +7,21 @@ import (
 	"rpc/internal/config"
 	"rpc/internal/server"
 	"rpc/pkg/api/test"
+	"strconv"
 )
 
 func main() {
-	cfg, err := config.ParseConfig(".env")
+	cfg, err := config.ParseConfig("./config/.env")
 	if err != nil {
 		log.Fatalf("Failed to load config: %v", err)
+		return
 	}
+	log.Printf("Config loaded: %+v", cfg)
+
 	grpcserver := grpc.NewServer()
 	orderServer := server.NewServer()
 	test.RegisterOrderServiceServer(grpcserver, orderServer)
-	listener, err := net.Listen("tcp", cfg.Port)
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(cfg.Port))
 	if err != nil {
 		log.Fatalf("failed to listen on %v:%v\n", listener.Addr(), err)
 	}
---
+++ internal/config/config.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+	"time"
+)
+
+type Config struct {
+	Port     int           `env:"GRPC_PORT" env-default:"50051"`
+	LogLevel string        `env:"LOG_LEVEL" env-default:"info"`
+	Timeout  time.Duration `env:"HTTP_TIMEOUT" env-default:"30s"`
+}
+
+func ParseConfig(path string) (*Config, error) {
+	cfg := &Config{}
+	if err := cleanenv.ReadConfig(path, cfg); err != nil {
+		return nil, fmt.Errorf("failed to parse config: %w", err)
+	}
+	return cfg, nil
+}
